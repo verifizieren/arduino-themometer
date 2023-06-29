eda36e84e1397a70562c65b9ecfb145500e972dc
Max <135263966+verifizieren@users.noreply.github.com>
2023-06-29 16:52:00 +0200

    Update them.ino
    Added humidity.

--- them.ino
+++ them.ino
@@ -1,11 +1,19 @@
 #include <LiquidCrystal_PCF8574.h>  // Bibliothek LCD-Anzeige.
+#include <DHT.h>
 #define LM35 A2                     // Analoger Eingang mit 10-Bit AD-Wandler (Temperatursensor LM35).
+
+#define DHTPIN D4 // Anschluss DHT11-Sensormodul.
+#define DHTTYPE DHT11 // Typ DHT 11.
+
+
 #define A 0x01
 #define LED_bl D13
 #define LED_rt D12
                       // Namen für die Zustände.
 LiquidCrystal_PCF8574 lcd(0x27);    // LCD-Adresse auf 0x27 für 16 Zeichen und 2 Zeilen
+DHT dht(DHTPIN, DHTTYPE); 
                                     // einschalten.
+unsigned int Feuchte, Temperatur;
 unsigned char zustand = 1;          // Zustandsvariable.
 unsigned short z;                   // Digitaler Ausgang AD-Wandler (Zahlenwert z).
 float temperatur;                   // Variable für die Temperatur.
@@ -22,22 +30,25 @@ void loop() {
     case A:
       z = analogRead(LM35);
       temperatur = float(((z * 3.3)/1023)*100);
+      Feuchte = dht.readHumidity();
+      Temperatur = dht.readTemperature();
+
       if (temperatur >= 30){
-        I2C_LCD("Temperatur:", "T = " + String(temperatur) + "C");
+        I2C_LCD("T = " + String(temperatur) + "C", "Humidity = " + String(Feuchte) + "%");
         blinker();
         delay(200);
       } else if (25 < temperatur < 30){
-        I2C_LCD("Temperatur:", "T = " + String(temperatur) + "C");
+        I2C_LCD("T = " + String(temperatur) + "C", "Humidity = " + String(Feuchte) + "%");
         digitalWrite(LED_bl, 0);
         digitalWrite(LED_rt, 100);
         delay(500);
       }else if (20 < temperatur < 25){
-        I2C_LCD("Temperatur:", "T = " + String(temperatur) + "C");
+        I2C_LCD("T = " + String(temperatur) + "C", "Humidity = " + String(Feuchte) + "%");
         digitalWrite(LED_bl, 100);
         digitalWrite(LED_rt, 100);
         delay(500);
       } else {
-        I2C_LCD("Temperatur:", "T = " + String(temperatur) + "C");
+        I2C_LCD("T = " + String(temperatur) + "C", "Humidity = " + String(Feuchte) + "%");
         digitalWrite(LED_bl, 100);
         digitalWrite(LED_rt, 0);
         delay(500);
